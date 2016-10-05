872beeafa22a6d85fd6a26947430f8fd4f04ea2c
faandreolli <faandreolli@gmail.com>
2016-10-05 16:54:24 -0300

    Efeito em portifolios

---
+++ js/portifolio.js
@@ -0,0 +1,67 @@
+	function todos_port()
+		
+		{
+			//document.getElementsByClassName("img").style.visibility="show";
+			document.getElementById("multi1").style.display="block";
+			document.getElementById("multi2").style.display="block";
+			document.getElementById("loja1").style.display="block";
+			document.getElementById("app1").style.display="block";
+			document.getElementById("app2").style.display="block";
+			document.getElementById("banner1").style.display="block";
+		}	
+	
+	function multi_port()
+		
+		{
+			//document.getElementsByClassName("img").style.visibility="show";
+			document.getElementById("multi1").style.display="block";
+			document.getElementById("multi2").style.display="block";
+			document.getElementById("loja1").style.display="none";
+			document.getElementById("app1").style.display="none";
+			document.getElementById("app2").style.display="none";
+			document.getElementById("banner1").style.display="none";
+		}
+	
+	
+	function loja_port()
+		
+		{
+			//document.getElementsByClassName("img").style.visibility="show";
+			document.getElementById("multi1").style.display="none";
+			document.getElementById("multi2").style.display="none";
+			document.getElementById("loja1").style.display="block";
+			document.getElementById("app1").style.display="none";
+			document.getElementById("app2").style.display="none";
+			document.getElementById("banner1").style.display="none";
+		}
+	
+	function app_port()
+		
+		{
+			//document.getElementsByClassName("img").style.visibility="show";
+			document.getElementById("multi1").style.display="none";
+			document.getElementById("multi2").style.display="none";
+			document.getElementById("loja1").style.display="none";
+			document.getElementById("app1").style.display="block";
+			document.getElementById("app2").style.display="block";
+			document.getElementById("banner1").style.display="none";
+		}
+		
+		function banner_port()
+		
+		{
+			//document.getElementsByClassName("img").style.visibility="show";
+			document.getElementById("multi1").style.display="none";
+			document.getElementById("multi2").style.display="none";
+			document.getElementById("loja1").style.display="none";
+			document.getElementById("app1").style.display="none";
+			document.getElementById("app2").style.display="none";
+			document.getElementById("banner1").style.display="block";
+		}
+	
+	
+	
+	
+
+
+	
